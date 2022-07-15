e265a118bbb0b3edf94c02a79971fe0c2305f1b0
crustydev <ademola1145@gmail.com>
2022-07-15 13:01:10 +0100

    Write doc comments for account safety checks

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -1,14 +1,9 @@
 use anchor_lang::prelude::*;
-//use whitelist::{self, WhitelistConfig, Wallet};
-use whitelist::{self};
 use whitelist::cpi::accounts::{
-    CreateWhitelist,
-    CheckWallet,
-    AddWallet,
-    RemoveWallet,
-    SetAuthority,
+    AddWallet, CheckWallet, CreateWhitelist, RemoveWallet, SetAuthority,
 };
 use whitelist::program::Whitelist;
+use whitelist::{self};
 
 declare_id!("Bt86r9ytWScWYfVd6sNefoVcow1TRw3ncvYPQ93BZgWP");
 
@@ -16,7 +11,6 @@ const DISCRIMINATOR_LENGTH: usize = 8;
 const PUBKEY_LENGTH: usize = 32;
 const UNSIGNED64_LENGTH: usize = 8;
 const UNSIGNED8_LENGTH: usize = 8;
-
 
 #[program]
 pub mod counter {
@@ -34,18 +28,19 @@ pub mod counter {
     pub fn create_counter_whitelist(ctx: Context<CreateCounterWhitelist>) -> Result<()> {
         let counter = &mut ctx.accounts.counter;
 
-        require_keys_eq!(counter.whitelist, Pubkey::default(),
-            CounterError::AssociatedWhitelistAlreadyExists);
+        require_keys_eq!(
+            counter.whitelist,
+            Pubkey::default(),
+            CounterError::AssociatedWhitelistAlreadyExists
+        );
 
         counter.whitelist = ctx.accounts.whitelist_account.key();
 
         let create_cpi_ctx = ctx.accounts.into_create_whitelist_context();
         _ = whitelist::cpi::create_whitelist(create_cpi_ctx);
 
-        
         let set_authority_ctx = ctx.accounts.into_set_authority_context();
-        _ = whitelist::cpi::set_authority(set_authority_ctx, 
-            ctx.accounts.counter.key());
+        _ = whitelist::cpi::set_authority(set_authority_ctx, ctx.accounts.counter.key());
 
         Ok(())
     }
@@ -64,16 +59,19 @@ pub mod counter {
         let authority_key = ctx.accounts.authority.key();
         let bump = *ctx.bumps.get("counter").unwrap();
 
-        let counter_seeds = &["counter".as_bytes().as_ref(), authority_key.as_ref(), &[bump]];
+        let counter_seeds = &[
+            "counter".as_bytes().as_ref(),
+            authority_key.as_ref(),
+            &[bump],
+        ];
         let signer = &[&counter_seeds[..]];
-        let cpi_ctx = ctx.accounts.into_check_wallet_context()
-            .with_signer(signer);
+        let cpi_ctx = ctx.accounts.into_check_wallet_context().with_signer(signer);
 
         match whitelist::cpi::check_wallet(cpi_ctx, user.key()) {
             Ok(()) => {
                 ctx.accounts.counter.count = present_count.checked_add(1).unwrap();
                 Ok(())
-            },
+            }
             Err(_) => Err(CounterError::UserIsNotWhitelisted.into()),
         }
     }
@@ -83,7 +81,11 @@ pub mod counter {
         let authority_key = ctx.accounts.authority.key();
         let bump = *ctx.bumps.get("counter").unwrap();
 
-        let counter_seeds = &["counter".as_bytes().as_ref(), authority_key.as_ref(), &[bump]];
+        let counter_seeds = &[
+            "counter".as_bytes().as_ref(),
+            authority_key.as_ref(),
+            &[bump],
+        ];
         let signer = &[&counter_seeds[..]];
 
         let cpi_ctx = ctx.accounts.into_add_wallet_context().with_signer(signer);
@@ -97,17 +99,22 @@ pub mod counter {
         let authority_key = ctx.accounts.authority.key();
         let bump = *ctx.bumps.get("counter").unwrap();
 
-        let counter_seeds = &["counter".as_bytes().as_ref(), authority_key.as_ref(), &[bump]];
+        let counter_seeds = &[
+            "counter".as_bytes().as_ref(),
+            authority_key.as_ref(),
+            &[bump],
+        ];
         let signer = &[&counter_seeds[..]];
 
-        let cpi_ctx = ctx.accounts.into_remove_wallet_context()
+        let cpi_ctx = ctx
+            .accounts
+            .into_remove_wallet_context()
             .with_signer(signer);
 
         _ = whitelist::cpi::remove_wallet(cpi_ctx, wallet_address);
         Ok(())
     }
 }
-
 
 #[derive(Accounts)]
 pub struct CreateCounter<'info> {
@@ -116,10 +123,7 @@ pub struct CreateCounter<'info> {
     #[account(
         init,
         seeds = ["counter".as_bytes().as_ref(), authority.key().as_ref()],
-        bump,
-        payer = authority, 
-        space = Counter::LEN,
-
+        bump, payer = authority, space = Counter::LEN
     )]
     counter: Account<'info, Counter>,
     system_program: Program<'info, System>,
@@ -137,39 +141,13 @@ pub struct CreateCounterWhitelist<'info> {
     )]
     counter: Account<'info, Counter>,
     #[account(mut, signer)]
-    /// CHECK: Checks are done by the whitelist program via CPI
+    /// CHECK: Verification done by CPI to whitelist program
     whitelist_account: AccountInfo<'info>,
     whitelist_program: Program<'info, Whitelist>,
     system_program: Program<'info, System>,
 }
 
-impl <'info> CreateCounterWhitelist <'info> {
-    pub fn into_create_whitelist_context(&self) -> CpiContext<'_, '_, '_, 'info, CreateWhitelist<'info>> {
-        let whitelist_program = self.whitelist_program.to_account_info();
-
-        let create_whitelist_accounts = CreateWhitelist {
-            authority: self.authority.to_account_info(),
-            whitelist_config: self.whitelist_account.to_account_info(),
-            system_program: self.system_program.to_account_info(),
-        };
-    
-        CpiContext::new(whitelist_program, create_whitelist_accounts)
-    }
-
-    pub fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
-        let whitelist_program = self.whitelist_program.to_account_info();
-
-        let set_authority_accounts = SetAuthority {
-            whitelist_config: self.whitelist_account.to_account_info(),
-            current_authority: self.authority.to_account_info(),
-        };
-
-        CpiContext::new(whitelist_program, set_authority_accounts)
-    }
-}
-
 #[derive(Accounts)]
-//#[instruction(wallet_address: Pubkey)]
 pub struct GrantAccess<'info> {
     #[account(mut)]
     authority: Signer<'info>,
@@ -181,27 +159,13 @@ pub struct GrantAccess<'info> {
     )]
     counter: Account<'info, Counter>,
     #[account(mut)]
-    /// CHECK: todo
+    /// CHECK: Verification done by CPI to whitelist program
     wallet_pda: AccountInfo<'info>,
     #[account(mut)]
-    /// CHECK: todo
+    /// CHECK: Verification done by CPI to whitelist program
     whitelist: AccountInfo<'info>,
     whitelist_program: Program<'info, Whitelist>,
     system_program: Program<'info, System>,
-}
-
-impl <'info> GrantAccess <'info> {
-    pub fn into_add_wallet_context(&self) -> CpiContext<'_, '_, '_, 'info, AddWallet<'info>> {
-        let whitelist_program = self.whitelist_program.to_account_info();
-        let add_wallet_accounts = AddWallet {
-            fee_payer: self.authority.to_account_info(),
-            whitelist_config: self.whitelist.to_account_info(),
-            wallet_pda: self.wallet_pda.to_account_info(),
-            authority: self.counter.to_account_info(),
-            system_program: self.system_program.to_account_info(),
-        };
-        CpiContext::new(whitelist_program, add_wallet_accounts)
-    }
 }
 
 #[derive(Accounts)]
@@ -217,27 +181,13 @@ pub struct RetractAccess<'info> {
     )]
     counter: Account<'info, Counter>,
     #[account(mut)]
-    /// CHECK: todo
+    /// CHECK: Verification done by CPI to whitelist program
     wallet_pda: AccountInfo<'info>,
-    /// CHECK: todo
+    /// CHECK: Verification done by CPI to whitelist program
     #[account(mut)]
     whitelist: AccountInfo<'info>,
     whitelist_program: Program<'info, Whitelist>,
 }
-
-impl <'info> RetractAccess<'info> {
-    pub fn into_remove_wallet_context(&self) -> CpiContext<'_, '_, '_,  'info, RemoveWallet<'info>> {
-        let whitelist_program = self.whitelist_program.to_account_info();
-        let remove_wallet_accounts = RemoveWallet {
-            whitelist_config: self.whitelist.to_account_info(),
-            wallet_pda: self.wallet_pda.to_account_info(),
-            authority: self.counter.to_account_info(),
-            refund_wallet: self.authority.to_account_info(),
-        };
-        CpiContext::new(whitelist_program, remove_wallet_accounts)
-    }
-}
-
 
 #[derive(Accounts)]
 pub struct ResetWhitelist<'info> {
@@ -256,6 +206,7 @@ pub struct UpdateCounter<'info> {
     user: Signer<'info>,
     /// CHECK: We validate this account by comparing it to counter.authority
     #[account(constraint = authority.key() == counter.authority)]
+    /// CHECK: We do not read or write from this account
     authority: AccountInfo<'info>,
     #[account(
         mut,
@@ -266,14 +217,68 @@ pub struct UpdateCounter<'info> {
         constraint = counter.whitelist != Pubkey::default()
     )]
     counter: Account<'info, Counter>,
-    /// CHECK: todo
+    /// CHECK: Verification done by CPI to whitelist program
     user_wallet_pda: AccountInfo<'info>,
-    /// CHECK: todo
+    /// CHECK: Verification done by CPI to whitelist program
     whitelist: AccountInfo<'info>,
     whitelist_program: Program<'info, Whitelist>,
 }
 
-impl<'info> UpdateCounter <'info> {
+impl<'info> CreateCounterWhitelist<'info> {
+    pub fn into_create_whitelist_context(
+        &self,
+    ) -> CpiContext<'_, '_, '_, 'info, CreateWhitelist<'info>> {
+        let whitelist_program = self.whitelist_program.to_account_info();
+
+        let create_whitelist_accounts = CreateWhitelist {
+            authority: self.authority.to_account_info(),
+            whitelist_config: self.whitelist_account.to_account_info(),
+            system_program: self.system_program.to_account_info(),
+        };
+
+        CpiContext::new(whitelist_program, create_whitelist_accounts)
+    }
+
+    pub fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let whitelist_program = self.whitelist_program.to_account_info();
+
+        let set_authority_accounts = SetAuthority {
+            whitelist_config: self.whitelist_account.to_account_info(),
+            current_authority: self.authority.to_account_info(),
+        };
+
+        CpiContext::new(whitelist_program, set_authority_accounts)
+    }
+}
+
+impl<'info> GrantAccess<'info> {
+    pub fn into_add_wallet_context(&self) -> CpiContext<'_, '_, '_, 'info, AddWallet<'info>> {
+        let whitelist_program = self.whitelist_program.to_account_info();
+        let add_wallet_accounts = AddWallet {
+            fee_payer: self.authority.to_account_info(),
+            whitelist_config: self.whitelist.to_account_info(),
+            wallet_pda: self.wallet_pda.to_account_info(),
+            authority: self.counter.to_account_info(),
+            system_program: self.system_program.to_account_info(),
+        };
+        CpiContext::new(whitelist_program, add_wallet_accounts)
+    }
+}
+
+impl<'info> RetractAccess<'info> {
+    pub fn into_remove_wallet_context(&self) -> CpiContext<'_, '_, '_, 'info, RemoveWallet<'info>> {
+        let whitelist_program = self.whitelist_program.to_account_info();
+        let remove_wallet_accounts = RemoveWallet {
+            whitelist_config: self.whitelist.to_account_info(),
+            wallet_pda: self.wallet_pda.to_account_info(),
+            authority: self.counter.to_account_info(),
+            refund_wallet: self.authority.to_account_info(),
+        };
+        CpiContext::new(whitelist_program, remove_wallet_accounts)
+    }
+}
+
+impl<'info> UpdateCounter<'info> {
     pub fn into_check_wallet_context(&self) -> CpiContext<'_, '_, '_, 'info, CheckWallet<'info>> {
         let whitelist_program = self.whitelist_program.to_account_info();
         let check_wallet_accounts = CheckWallet {
@@ -290,13 +295,13 @@ pub struct Counter {
     authority: Pubkey,
     whitelist: Pubkey,
     count: u64,
-    bump: u8
+    bump: u8,
 }
 
 impl Counter {
-    const LEN: usize = DISCRIMINATOR_LENGTH + (PUBKEY_LENGTH) * 2 + UNSIGNED64_LENGTH + UNSIGNED8_LENGTH;
+    const LEN: usize =
+        DISCRIMINATOR_LENGTH + (PUBKEY_LENGTH) * 2 + UNSIGNED64_LENGTH + UNSIGNED8_LENGTH;
 }
-
 
 #[error_code]
 pub enum CounterError {
@@ -307,7 +312,10 @@ pub enum CounterError {
 
 // Checks that whitelist is not set to default pubkey
 fn whitelist_initialized(counter: &Counter) -> Result<()> {
-    require_keys_neq!(counter.whitelist, Pubkey::default(),
-        CounterError::WhitelistNotInitialized);
+    require_keys_neq!(
+        counter.whitelist,
+        Pubkey::default(),
+        CounterError::WhitelistNotInitialized
+    );
     Ok(())
 }
--- programs/whitelist/src/lib.rs
+++ programs/whitelist/src/lib.rs
@@ -2,7 +2,6 @@ use anchor_lang::prelude::*;
 
 declare_id!("6Ks63cD2xLXF6umVaSZLJY6ejxinDTJvjE3X3GtezUcM");
 
-
 #[program]
 pub mod whitelist {
     use super::*;
@@ -15,43 +14,31 @@ pub mod whitelist {
         Ok(())
     }
 
-    pub fn set_authority(
-        ctx: Context<SetAuthority>, 
-        new_authority: Pubkey
-    ) -> Result<()> {
-        let config = &mut ctx.accounts.whitelist_config;
-        config.authority = new_authority;
-        Ok(())
-    }
-
-    pub fn add_wallet(
-        ctx: Context<AddWallet>,
-        _wallet_address: Pubkey
-    ) -> Result<()> {
+    pub fn add_wallet(ctx: Context<AddWallet>, _wallet_address: Pubkey) -> Result<()> {
         let config = &mut ctx.accounts.whitelist_config;
         config.counter = config.counter.checked_add(1).unwrap();
         Ok(())
     }
 
-    pub fn check_wallet(
-        _ctx: Context<CheckWallet>,
-        _wallet_address: Pubkey
-    ) -> Result<()> {
+    pub fn check_wallet(_ctx: Context<CheckWallet>, _wallet_address: Pubkey) -> Result<()> {
         Ok(())
     }
-            
-    pub fn remove_wallet(
-        ctx: Context<RemoveWallet>,
-        _wallet_address: Pubkey,
-    ) -> Result<()> {
+
+    pub fn remove_wallet(ctx: Context<RemoveWallet>, _wallet_address: Pubkey) -> Result<()> {
         let config = &mut ctx.accounts.whitelist_config;
         config.counter = config.counter.checked_sub(1).unwrap();
+        Ok(())
+    }
+
+    pub fn set_authority(ctx: Context<SetAuthority>, new_authority: Pubkey) -> Result<()> {
+        let config = &mut ctx.accounts.whitelist_config;
+        config.authority = new_authority;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateWhitelist <'info> {
+pub struct CreateWhitelist<'info> {
     #[account(mut)]
     authority: Signer<'info>,
     #[account(
@@ -62,18 +49,6 @@ pub struct CreateWhitelist <'info> {
     whitelist_config: Account<'info, WhitelistConfig>,
     system_program: Program<'info, System>,
 }
-
-
-#[derive(Accounts)]
-pub struct SetAuthority<'info> {
-    #[account(
-        mut,
-        constraint = whitelist_config.authority == current_authority.key(),
-    )]
-    whitelist_config: Account<'info, WhitelistConfig>,
-    current_authority: Signer<'info>
-}
-
 
 #[derive(Accounts)]
 #[instruction(wallet_address: Pubkey)]
@@ -91,20 +66,19 @@ pub struct AddWallet<'info> {
         space = 8,
     )]
     wallet_pda: Account<'info, Wallet>,
-    /// CHECK: todo
+    /// CHECK: We do not read or write from this account
     authority: AccountInfo<'info>,
     #[account(mut)]
     fee_payer: Signer<'info>,
     system_program: Program<'info, System>,
 }
 
-
 #[derive(Accounts)]
 #[instruction(wallet_address: Pubkey)]
 pub struct CheckWallet<'info> {
     #[account(has_one = authority)]
     whitelist_config: Account<'info, WhitelistConfig>,
-    /// CHECK: CheckWallet authority will be a PDA 
+    /// CHECK: We do not read or write from this account
     authority: AccountInfo<'info>,
     #[account(
         seeds = [whitelist_config.key().as_ref(), wallet_address.key().as_ref()],
@@ -115,7 +89,7 @@ pub struct CheckWallet<'info> {
 
 #[derive(Accounts)]
 #[instruction(wallet_address: Pubkey)]
-pub struct RemoveWallet <'info> {
+pub struct RemoveWallet<'info> {
     #[account(
         mut,
         has_one = authority,
@@ -128,13 +102,21 @@ pub struct RemoveWallet <'info> {
         close = refund_wallet,
     )]
     wallet_pda: Account<'info, Wallet>,
-    /// CHECK: todo
+    /// CHECK: We do not read or write from this account
     authority: AccountInfo<'info>,
     #[account(mut)]
     refund_wallet: Signer<'info>,
 }
 
-
+#[derive(Accounts)]
+pub struct SetAuthority<'info> {
+    #[account(
+        mut,
+        constraint = whitelist_config.authority == current_authority.key(),
+    )]
+    whitelist_config: Account<'info, WhitelistConfig>,
+    current_authority: Signer<'info>,
+}
 
 #[account]
 pub struct WhitelistConfig {
